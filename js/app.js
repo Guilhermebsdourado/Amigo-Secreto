0a8292e166318f36834946d44b7b6ae67c90bbfa
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-06-03 12:52:17 -0300

    Amigo Secreto

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ let amigos = [];
 
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
+    if(amigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+    if(amigos.includes(amigo.value)) {
+        alert('Nome ja adicionado!');
+        return;
+    }
     let lista = document.getElementById('lista-amigos');
     amigos.push(amigo.value);
     if (lista.textContent == '') {
@@ -12,11 +21,22 @@ function adicionar(){
     amigo.value = '';
 }
 function sortear() {
+if (amigos.length < 4) {
+    alert('Adicione pelo menos 4 amigos!');
+    return;
+}
+
     embaralhar(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
     for(let i = 0;i < amigos.length; i++ ) {
-        sorteio.innerHTML = sorteio.innerHTML + amigos [i] + ' --> ' + amigos [i + 1] + '<br>';
+
+        if (i == amigos.length -1){
+           sorteio.innerHTML = sorteio.innerHTML + amigos [i] + ' --> ' + amigos [0] + '<br>'; 
+        } else {
+           sorteio.innerHTML = sorteio.innerHTML + amigos [i] + ' --> ' + amigos [i + 1] + '<br>'; 
+        }
+        
     }
 }
 function embaralhar(lista){
@@ -26,4 +46,10 @@ function embaralhar(lista){
         [lista[indice -1], lista[indiceAleatorio]] =
             [lista[indiceAleatorio], lista[indice - 1]];
     }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
